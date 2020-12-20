2e7812adf6e5ca2698dd3d6115b90bf22c95cc8c
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2020-12-20 19:55:01 +0500

    |add| add configurations for production

--- fe-angular/src/app/graphql.module.ts
+++ fe-angular/src/app/graphql.module.ts
@@ -3,7 +3,9 @@ import { APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 
-const uri = 'http://localhost/api/graphql'; // <-- add the URL of the GraphQL server here
+import { environment } from '../environments/environment';
+
+const uri = environment.backendUrl + '/api/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({ uri }),
